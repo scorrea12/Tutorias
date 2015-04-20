6163af2b3af52cb47706ba31c90e8d7c4bc80d01
Didage <didage@gmail.com>
2015-04-20 14:13:52 -0500

    Cambios en las librerías

--- app/app.js
+++ app/app.js
@@ -4,9 +4,14 @@
 angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
-  'myApp.view2',
+  'myApp.Tomar',
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+
+  $routeProvider
+      .otherwise({redirectTo: '/view1'});
+      .when('/Tomar', {
+            templateUrl:'/Tomar/view2.html'
+        });
 }]);
